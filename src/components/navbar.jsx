0245aa569279b16f18b8e884b91ba6e7da029aab
fishyimm <sedthasith.r@gmail.com>
2019-01-21 23:40:24 +0700

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+// class NarBar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-light bg-light">
+//         <a className="navbar-brand" href="#">
+//           Navbar
+//           <span className="badge badge-pill badge-secondary">
+//             {this.props.totalCounters}
+//           </span>
+//         </a>
+//       </nav>
+//     );
+//   }
+// }
+
+const NarBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+// export default NarBar;
+
+export default NarBar;
